231880b5f65cc6ec1f0ee211273c8361bf62b5d8
nishit1606 <nishit1606@gmail.com>
2021-06-15 14:36:11 +0530

    added navigatior

---
+++ lib/category_meals.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('The Recipy'),
+      ),
+      body: Center(
+        child: Text('The Recipy for the category'),
+      ),
+    );
+  }
+}
