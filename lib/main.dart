64640faf5f35cc3da1628aa575ebe5b369da8703
nishit1606 <nishit1606@gmail.com>
2021-06-15 15:22:49 +0530

    used named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './category_meals.dart';
 import './categories.dart';
 
 void main() => runApp(MyApp());
@@ -15,22 +16,24 @@ class MyApp extends StatelessWidget {
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-            // ignore: deprecated_member_use
             body1: TextStyle(
               color: Color.fromRGBO(20, 51, 51, 1),
             ),
-            // ignore: deprecated_member_use
             body2: TextStyle(
               color: Color.fromRGBO(20, 51, 51, 1),
             ),
-            // ignore: deprecated_member_use
             title: TextStyle(
               fontSize: 20,
               fontFamily: 'RobotoCondensed',
               fontWeight: FontWeight.bold,
             )),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+      initialRoute: '/', // default is '/'
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
