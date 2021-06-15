4300f0c2de820ffb041f3ca694845d8c87458bd2
nishit1606 <nishit1606@gmail.com>
2021-06-15 14:46:25 +0530

    changed bar name

--- lib/category_meals.dart
+++ lib/category_meals.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipy'),
+        title: Text(categoryTitle),
       ),
       body: Center(
-        child: Text('The Recipy for the category'),
+        child: Text(
+          'The Recipes For The Category!',
+        ),
       ),
     );
   }
